08ff8b2aa47199c647504227f2fb31deb411889a
Joseph Kwan <jtkwan91@gmail.com>
2022-01-23 00:02:36 -0500

    dropdown fix

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -16,9 +16,9 @@ function Header() {
       <button className='header__dropdown'><img className='header__dropdown-img' src={dropdownIcon} alt="dropdown icon" onClick={() => setToggleDropdown(!toggleDropdown)}/></button>
       {toggleDropdown && (
       <div className={`header__dropdown-menu fade-top`}>
-        <Link className='header__dropdown-menu--item' to='/characters'>Character List</Link>
-        <Link className='header__dropdown-menu--item' to='/handbook'>Player's Handbook</Link>
-        <Link className='header__dropdown-menu--item' to='/'>Sign-out</Link>
+        <Link className='header__dropdown-menu--item' to='/characters' onClick={() => setToggleDropdown(!toggleDropdown)}>Character List</Link>
+        <Link className='header__dropdown-menu--item' to='/handbook' onClick={() => setToggleDropdown(!toggleDropdown)}>Player's Handbook</Link>
+        <Link className='header__dropdown-menu--item' to='/' onClick={() => setToggleDropdown(!toggleDropdown)}>Sign-out</Link>
       </div>)}
       </div>
       <img className='header__logo' src={chest} alt="treasure-chest" />
